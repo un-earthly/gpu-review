f2f58239a8a50b113d08b8af14f60110d6c26469
un-earthly <vijayalamin@gmail.com>
2022-04-03 12:14:15 -0700

    home banner done, custom hook created, color improvements

--- src/Components/Utils/Button.js
+++ src/Components/Utils/Button.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button({ btnText, classList }) {
     return (
-        <button className={'px-8 py-4 ${classList}'}>{btnText}</button>
+        <button className={`px-8 text-xl py-4 ${classList}`}>{btnText}</button>
     )
 }
