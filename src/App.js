fbdbec704eee0e1866b1db2234f60ab6633abde5
un-earthly <vijayalamin@gmail.com>
2022-04-03 11:10:52 -0700

    component routing and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Reviews from './Components/Reviews/Reviews'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Blogs from './Components/Blogs/Blogs'
+import About from './Components/About/About'
+import Error from './Components/Error/Error'
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/dashbord' element={<Dashboard />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/About' element={<About />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,14 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className="md:flex justify-around items-center">
-            <div className="logo"></div>
-            <ul className="flex items-center justify-center">
-                <NavLink activeclassname='active' className=''>1</NavLink>
-                <NavLink activeclassname='active' className=''>2</NavLink>
-                <NavLink activeclassname='active' className=''>3</NavLink>
-                <NavLink activeclassname='active' className=''>4</NavLink>
-            </ul>
+        <nav className="flex justify-center items-center">
+            <NavLink to='/' activeclassname='active' className='uppercase mx-3 my-2 py-1 px-3 border-b-2 hover:border-teal-400 hover:text-teal-600 duration-500 font-bold'>Home</NavLink>
+            <NavLink to='/reviews' activeclassname='active' className='uppercase mx-3 my-2 py-1 px-3 border-b-2 hover:border-teal-400 hover:text-teal-600 duration-500 font-bold'>reviews</NavLink>
+            <NavLink to='/dashboard' activeclassname='active' className='uppercase mx-3 my-2 py-1 px-3 border-b-2 hover:border-teal-400 hover:text-teal-600 duration-500 font-bold'>dashboard</NavLink>
+            <NavLink to='/blogs' activeclassname='active' className='uppercase mx-3 my-2 py-1 px-3 border-b-2 hover:border-teal-400 hover:text-teal-600 duration-500 font-bold'>blogs</NavLink>
+            <NavLink to='/about' activeclassname='active' className='uppercase mx-3 my-2 py-1 px-3 border-b-2 hover:border-teal-400 hover:text-teal-600 duration-500 font-bold'>about</NavLink>
         </nav>
     )
 }
