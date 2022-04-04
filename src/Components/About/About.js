d6f3e496d20b9c3d4ba9f36d27d238a0b872de0e
un-earthly <vijayalamin@gmail.com>
2022-04-04 04:04:50 -0700

    error page added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Article from '../Article/Article'
 
 export default function About() {
     return (
-        <div className='min-h-screen'>
-            <p className='capitalize'>This is web application showing users reviews about new rtx 3080 graphics card</p>
+        <div>
+            <Article
+                heading={' About this site'}
+                paragraph={`This is web application showing users reviews about new rtx 3080 graphics card.i've used react to make this application a single page application. used react router dom to navigate between deiffernt routes.tailwind css to design this application, recharts for the charts show in dashboard area,fonts from google fonts to make the font better.`} />
         </div>
     )
 }
--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
     return (
-        <div className='h-screen text'>
+        // <div className='h-[87.7vh] text-center flex flex-col items-center justify-center bg-contain bg-center  bg-no-repeat bg-[url("https://img.freepik.com/free-vector/404-error-page-found-natural-fantasy-illustration-concept-web-missing-page_111925-144.jpg?w=740")]'>
+        <div className='h-[87.7vh] text-center flex flex-col items-center justify-center'> <h1 className='capitalize text-7xl text-[red]'> 404! </h1>
+            <p className='text-4xl'>possible routes</p>
+            <ul className='text-[orange] '>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/reviews'>Reviews</Link>
+                </li>
+                <li>
+                    <Link to='/dashboard'>Dashboard</Link>
+                </li>
+                <li>
+                    <Link to='/blogs'>Blogs</Link>
+                </li>
+                <li>
 
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </div>
+        // </div>
     )
 }
